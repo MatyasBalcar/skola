36abb5546191645f0314d5c3b2a4c979f6cf512b
@balcarma <matyasbalcar0@gmail.com>
2022-11-29 20:34:57 +0100

    opravene

--- main.py
+++ main.py
@@ -1,27 +1,28 @@
 def cteni(cesta):
     try:
         f = open(cesta, "r")
-        obsah = f.read()
-        f.close()
-        return obsah
+        
     except FileNotFoundError:
         print(f"soubor {cesta} neexistuje")
         return []
-cteni("soubor.txt")
+    obsah = f.read()
+    f.close()
+    return obsah
+print(cteni("soubor.txt"))
 
 
 def hledat(klic, hodnota,slovnik):
     try:
         hodnota_=slovnik[klic]
-        if hodnota_==hodnota:
-            print("hodnota se shoduje")
-            return True
-        else:
-            print("hodnota se neshoduje")
+        
     except KeyError:
         print("klic neexistuje")
         return False
-
+    if hodnota_==hodnota:
+        print("hodnota se shoduje")
+        return True
+    else:
+        print("hodnota se neshoduje")
 hledat("ahoj","cus",{"ahoj":"cus"})
 
 def test():
